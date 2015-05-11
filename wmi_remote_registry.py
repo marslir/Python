ee3ad7a9d624a3b78172f67fbd3fa9fdcb275b97
marslir <marslir.knowsnothing@gmail.com>
2015-05-11 16:21:13 +0800

    Update wmi_remote_registry.py

--- wmi_remote_registry.py
+++ wmi_remote_registry.py
@@ -1,5 +1,28 @@
 import xlrd
-import _winreg
-c=wmi.WMI(computer="172.20.10.2",user="LIR",password="940914").StdRegProv
-results,names = c.GetStringValue(hDefKey=2147483649,sSubKeyName=r"Software\\360desktop",sValueName="appinstall1")#r"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\WindowsUpdate\\Auto Update\\Results\\Install",sValueName="LastSuccessTime")
-print names
+import os
+import wmi
+fname = "check.xlsx"
+bk = xlrd.open_workbook(fname)
+shxrange = range(bk.nsheets)
+try:
+  sh = bk.sheet_by_name("Sheet1")
+except:
+  print "no sheet in %s named Sheet1" % fname
+nrows = sh.nrows
+ncols = sh.ncols
+for i in range(0,nrows):
+  computer = sh.cell_value(i,0)
+  user = sh.cell_value(i,1)
+  password = sh.cell_value(i,2)
+  f = open("./log/history.log", "a")
+  conn=wmi.WMI(computer=computer,user=user,password=password,namespace="root/default").StdRegProv
+  f.write("IP address " + computer + "connected\n")
+  results,detect = conn.GetExpandedStringValue(hDefKey=2147483650,sSubKeyName=r"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\WindowsUpdate\\Auto Update\\Results\\Detect",sValueName="LastSuccessTime")
+  f.write("Last Detect Time :\n" + detect + "\n")
+  results,download = conn.GetExpandedStringValue(hDefKey=2147483650,sSubKeyName=r"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\WindowsUpdate\\Auto Update\\Results\\Detect",sValueName="LastSuccessTime")
+  f.write("Last Detect Time :\n" + download + "\n")
+  results,install = conn.GetExpandedStringValue(hDefKey=2147483650,sSubKeyName=r"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\WindowsUpdate\\Auto Update\\Results\\Detect",sValueName="LastSuccessTime")
+  f.write("Last Detect Time :\n" + install + "\n")
+  timenow=time.strftime('%a,%d,%b,%Y,%H:%M:%S',time.localtime())
+  f.wirte("Checked at local time:\n",timenow)
+  
